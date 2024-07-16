04c3ab06651e1a5f0bd2880dbe74fa4bccacad31
Noella Spitz <noella.spitz@gmail.com>
2024-07-16 22:43:14 +0700

    Create script and pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name   : "blue2020-website",
+    script : "npm run start",
+    env_production: {
+      NODE_ENV: "production"
+   },
+  }]
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
 import { Source_Sans_3, Roboto_Mono } from "next/font/google";
 import { ReactNode } from "react";
 import { Header } from "./components/Header";
@@ -24,6 +26,12 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en" className={`${sourceSans.variable} ${robotoMono.variable}`}>
       <body>
+        <Script id="mautic">{`(function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+          w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+          m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://crm.blue202labs.com/mtc.js','mt');
+  
+      mt('send', 'pageview');`}</Script>
         <Header />
         <main>{children}</main>
         <Footer />

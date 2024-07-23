524785ed23de28262825af3d4d6cac3258224f95
Noella Spitz <noella.spitz@gmail.com>
2024-07-24 02:44:38 +0700

    Update solutions

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -12,19 +12,7 @@ const SolutionsPage = () => {
       />
       <div className="max-w-4xl mx-auto md:py-10 flex flex-col gap-10 pb-40">
         <div className="flex flex-col justify-between h-full pt-20 pb-24 px-10 gap-10 md:gap-10 md:p-20">
-          <div className="flex flex-col gap-4 h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
-            <div className="flex flex-col gap-6 md:w-1/2 pr-6">
-              <h4 className="font-bold text-2xl py-2 md:py-0">Websites</h4>
-              <p className="text-lg text-slate-500">
-                We love making beautiful, responsive websites.
-              </p>
-            </div>
-            <div className="text-slate-600 font-body-sans text-xl flex flex-col gap-2 md:w-1/2 pt-8 md:pt-0 md:pl-10 border-t-2 md:border-t-0 border-t-blue-400 md:border-l-4 border-l-blue-500 h-full">
-              <Anchor route="/solutions/websites">Custom websites</Anchor>
-              <Anchor route="/solutions/wordpress">Wordpress sites</Anchor>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
+          <div className="flex flex-col gap-4 min-h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
             <div className="flex flex-col gap-6 md:w-1/2 pr-6">
               <h4 className="font-bold text-2xl py-2 md:py-0">Web Apps</h4>
               <p className="text-lg text-slate-500">
@@ -40,7 +28,51 @@ const SolutionsPage = () => {
               <Anchor route="/solutions/hosting">Hosting</Anchor>
             </div>
           </div>
-          <div className="flex flex-col gap-4 h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
+          <div className="flex flex-col gap-4 min-h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
+            <div className="flex flex-col gap-6 md:w-1/2 pr-6">
+              <h4 className="font-bold text-2xl py-2 md:py-0">For Startups</h4>
+              <p className="text-lg text-slate-500">
+                Our passion is bringing innovative ideas to life.
+              </p>
+            </div>
+            <div className="text-slate-600 font-body-sans text-xl flex flex-col gap-2 md:w-1/2 pt-8 md:pt-0 md:pl-10 border-t-2 md:border-t-0 border-t-blue-400 md:border-l-4 border-l-blue-500 h-full">
+              <Anchor route="/solutions/startup-development-package">
+                Startup Development Package
+              </Anchor>
+              <Anchor route="/solutions/mvp-development">
+                MVP Development
+              </Anchor>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 min-h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
+            <div className="flex flex-col gap-6 md:w-1/2 pr-6">
+              <h4 className="font-bold text-2xl py-2 md:py-0">Websites</h4>
+              <p className="text-lg text-slate-500">
+                We love making beautiful, responsive websites.
+              </p>
+            </div>
+            <div className="text-slate-600 font-body-sans text-xl flex flex-col gap-2 md:w-1/2 pt-8 md:pt-0 md:pl-10 border-t-2 md:border-t-0 border-t-blue-400 md:border-l-4 border-l-blue-500 h-full">
+              <Anchor route="/solutions/websites">Custom websites</Anchor>
+              <Anchor route="/solutions/wordpress">Wordpress sites</Anchor>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 min-h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
+            <div className="flex flex-col gap-6 md:w-1/2 pr-6">
+              <h4 className="font-bold text-2xl py-2 md:py-0">
+                Mobile Development
+              </h4>
+              <p className="text-lg text-slate-500">Why stop at websites?</p>
+            </div>
+            <div className="text-slate-600 font-body-sans text-xl flex flex-col gap-2 md:w-1/2 pt-8 md:pt-0 md:pl-10 border-t-2 md:border-t-0 border-t-blue-400 md:border-l-4 border-l-blue-500 h-full">
+              <Anchor route="/solutions/mobile-app-development">
+                Mobile App Development
+              </Anchor>
+              <Anchor route="/solutions/rapid-prototyping">
+                Rapid Prototyping
+              </Anchor>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 min-h-96 md:flex-row justify-between bg-white p-10 pb-20 md:p-20 rounded-xl drop-shadow-sm border">
             <div className="flex flex-col gap-6 md:w-1/2 pr-6">
               <h4 className="font-bold text-2xl py-2 md:py-0">
                 UI/UX & Marketing
